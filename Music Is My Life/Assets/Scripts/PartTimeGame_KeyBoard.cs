b3d25f84ece89c11808ee2dd929fd2f1c9d20ccd
HongYeonLee <llhongyan@naver.com>
2024-01-11 19:52:24 +0900

    알바게임 옮김

---
+++ Music Is My Life/Assets/Scripts/PartTimeGame_KeyBoard.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PartTimeGame_KeyBoard : MonoBehaviour
+{
+    public KeyCode assignedKey; //객체의 key
+    public Sprite KeyBoard_0; //좌
+    public Sprite KeyBoard_1; //우
+    public Sprite KeyBoard_2; //하
+    public Sprite KeyBoard_3; //상
+    public Sprite SpaceSprite; //스페이스바
+    public int objectIndex;
+    int keyState = 0; //키보드가 눌렸는지 체크하는 변수
+
+    public void SetKeySprite(KeyCode key, int index)
+    {
+        assignedKey = key; //이 스프라이트의 키를 저장
+        objectIndex = index;
+        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+        switch (assignedKey)
+        {
+            case KeyCode.LeftArrow:
+                spriteRenderer.sprite = KeyBoard_0;
+                break;
+            case KeyCode.RightArrow:
+                spriteRenderer.sprite = KeyBoard_1;
+                break;
+            case KeyCode.DownArrow:
+                spriteRenderer.sprite = KeyBoard_2;
+                break;
+            case KeyCode.UpArrow:
+                spriteRenderer.sprite = KeyBoard_3;
+                break;
+            case KeyCode.Space:
+                spriteRenderer.sprite = SpaceSprite;
+                break;
+        }
+    }
+
+    void Update()
+    {
+        if (objectIndex == PartTimeGame.turn)
+        {
+            if (Input.GetKeyDown(assignedKey))
+            {
+                keyState = 1;
+            }
+            if (keyState == 1 && !Input.anyKeyDown) //한번 입력되고 다른 키가 입력되지 않을 때
+            {
+                PartTimeGame.turn++;
+                Destroy(gameObject);
+                keyState = 0;
+            }
+            else if (Input.anyKeyDown && !Input.GetKeyDown(assignedKey) && !Input.GetMouseButtonDown(0) && !Input.GetMouseButtonDown(1)) 
+            {
+                keyState = 0;
+                foreach (GameObject keyboard in PartTimeGame.spawnedKeyboards)
+                {
+                    Destroy(keyboard);
+                }
+                PartTimeGame.spawnedKeyboards.Clear();
+                GameObject.FindWithTag("PartTimeGame").GetComponent<PartTimeGame>().moneyManager();
+                GameObject.FindWithTag("PartTimeGame").GetComponent<PartTimeGame>().increaseStageNum();
+                GameObject.FindWithTag("PartTimeGame").GetComponent<PartTimeGame>().SpawnKeyBoards();
+            }
+        }
+    }
+}
