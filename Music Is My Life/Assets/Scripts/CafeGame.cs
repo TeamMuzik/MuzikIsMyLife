af5ef6391fa7e471bea6f6aa08df6fd2e2de87ff
HongYeonLee <llhongyan@naver.com>
2024-01-18 11:16:27 +0900

    과일 10개, 맞게 클릭하면 사라짐

--- Music Is My Life/Assets/Scripts/CafeGame.cs
+++ Music Is My Life/Assets/Scripts/CafeGame.cs
@@ -6,41 +6,35 @@ public class CafeGame : MonoBehaviour
 {
     public GameObject[] FruitsPrefabs;
     public List<GameObject> TotalOrder = new List<GameObject>();
-    public GameObject[] SpawnedFruits;
+    public static int turn;
 
     void Start()
     {
         SpawnFruits();
     }
 
-    void Update()
-    {
-
-    }
-
     void SpawnFruits()
     {
+
+        turn = 0;
+        int index = 0;
         for (int i = 0; i < 12; i++)
         {
             int randNum = Random.Range(0, FruitsPrefabs.Length);
             GameObject RandomFruit = FruitsPrefabs[randNum];
             TotalOrder.Add(RandomFruit);
-            Debug.Log(TotalOrder[i]);
         }
 
-        for (int i = 0; i < 3; i++)
+        for (int i = 0; i < 6; i++)
         {
-            float xPos = 1.5f * i - 5;
-            Vector3 spawnPosition = new Vector3(xPos, 3, 0);
-            Instantiate(TotalOrder[i], spawnPosition, Quaternion.identity);
-        }
-
-
-        for (int i = 3; i < 6; i++)
-        {
-            float xPos = 2 + (i - 3) * 1.5f;
-            Vector3 spawnPosition = new Vector3(xPos, 3, 0);
-            Instantiate(TotalOrder[i], spawnPosition, Quaternion.identity);
+            if (i < 3)
+            {
+                float xPos = 1.5f * i - 5;
+                Vector3 spawnPosition = new Vector3(xPos, 3, 0);
+                GameObject FruitsObj = Instantiate(TotalOrder[i], spawnPosition, Quaternion.identity);
+                CafeGameFruits objIndex = FruitsObj.GetComponent<CafeGameFruits>();
+                objIndex.GetIndex(index++);
+            }
         }
     }
 }
--- Music Is My Life/Assets/Scripts/CafeGameFruits.cs
+++ Music Is My Life/Assets/Scripts/CafeGameFruits.cs
@@ -5,29 +5,42 @@ using UnityEngine;
 public class CafeGameFruits : MonoBehaviour
 {
     private CafeGame cafeGameInstance;
+    public int objIndex;
+    int ButtonState = 0;
+
     void Start()
     {
         cafeGameInstance = FindObjectOfType<CafeGame>();
     }
 
+    public void GetIndex (int num)
+    {
+        objIndex = num;
+    }
+
     void Update()
     {
-         if (Input.GetMouseButtonDown(0))
+        if (objIndex == CafeGame.turn)
         {
-            Vector2 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            RaycastHit2D hit = Physics2D.Raycast(pos, Vector2.zero, Mathf.Infinity);
-
-            for (int i = 0; i < 12; i ++)
+            if (Input.GetMouseButtonDown(0))
             {
+                ButtonState = 1;
+            }
+            if (ButtonState == 1 && !Input.GetMouseButtonDown(0) && !Input.GetMouseButtonDown(1))
+            {
+                Vector2 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                RaycastHit2D hit = Physics2D.Raycast(pos, Vector2.zero, Mathf.Infinity);
                 if (hit.collider != null)
                 {
                     GameObject clickedObject = hit.collider.gameObject;
-                    if (clickedObject.name == cafeGameInstance.TotalOrder[i].name)
+                    if (clickedObject.name == cafeGameInstance.TotalOrder[objIndex].name)
                     {
-                        Destroy(gameObject);                        
+                        CafeGame.turn++;
+                        Destroy(gameObject);
+                        ButtonState = 0;               
                     }
                 }
-            }
-        }       
+            }  
+        }  
     }
 }
