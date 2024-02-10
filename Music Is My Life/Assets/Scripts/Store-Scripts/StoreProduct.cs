428d820c98cf5f760ae1499f78f3989c39399194
seohyun-lee <applemint2003@gmail.com>
2024-02-10 19:35:17 +0900

    StoreProduct '잔고 부족' 효과 추가, 중복 구매 방지

--- Music Is My Life/Assets/Scripts/Store-Scripts/StoreProduct.cs
+++ Music Is My Life/Assets/Scripts/Store-Scripts/StoreProduct.cs
@@ -1,16 +1,19 @@
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System.Collections;
 
 public class StoreProduct : Furniture // Furniture을 상속받음
 {
     public int price; // 가격
     public bool replaceable;
-    public GameObject button;
+    public Button button;
+
+    private TextMeshProUGUI buttonText;
 
     public void BuyProduct()
     {
-        TextMeshProUGUI buttonText = button.GetComponentInChildren<TextMeshProUGUI>();
+        buttonText = button.GetComponentInChildren<TextMeshProUGUI>();
         if (StatusChanger.SpendMoney(price)) // 물건 구매 시도
         {
             if (replaceable)
@@ -23,12 +26,29 @@ public class StoreProduct : Furniture // Furniture을 상속받음
             if (buttonText != null)
             {
                 buttonText.text = "구매 완료"; // 텍스트를 변경하여 구매 완료로 설정
+                button.interactable = false;
             }
         }
         else
         {
-            buttonText.text = "잔고 부족"; // 텍스트를 변경
+            SetButtonTextAndColor("잔고 부족", "#FF262C"); // 텍스트를 변경, 글자를 빨간색으로
             Debug.Log("구매할 수 없습니다.");
+            StartCoroutine(MapPriceTextAfterDelay());
+        }
+    }
+    IEnumerator MapPriceTextAfterDelay()
+    {
+        yield return new WaitForSeconds(1f); // 1초 대기
+
+        // 1초 후에 가격으로 텍스트를 변경하고 검은색으로 설정
+        SetButtonTextAndColor(price.ToString()+"만원", "#323232");
+    }
+    private void SetButtonTextAndColor(string text, string colorCode)
+    {
+        if (buttonText != null)
+        {
+            buttonText.text = text; // 텍스트 변경
+            buttonText.color = ColorUtility.TryParseHtmlString(colorCode, out Color color) ? color : Color.black; // 컬러 코드를 Color로 변환하여 적용
         }
     }
 
