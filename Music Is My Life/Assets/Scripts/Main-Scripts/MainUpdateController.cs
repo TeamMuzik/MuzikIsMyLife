1afe0bd87f7d59ed9d4cf88fe4d14f636e10a9e9
seohyun-lee <applemint2003@gmail.com>
2024-02-17 03:08:24 +0900

    GoToEnding 개발
    - TooFamousToBeg 엔딩명 '나락도 락이다'로 교체
    - Ending-TooFamousToBeg 씬명 Ending-Narak으로 교체
    - MainUpdateController에 있던 엔딩 분기 코드 EndingSelector로 이동

--- Music Is My Life/Assets/Scripts/Main-Scripts/MainUpdateController.cs
+++ Music Is My Life/Assets/Scripts/Main-Scripts/MainUpdateController.cs
@@ -17,12 +17,17 @@ public class MainUpdateController : MonoBehaviour
         // 14일이 지나면 15일: 엔딩으로 이동
         if (PlayerPrefs.GetInt("Dday") >= PlayerPrefs.GetInt("EndDday")) // EndDday: 15
         {
-            GoToEnding();
+            SceneMove sceneMove = gameObject.AddComponent<SceneMove>();
+            sceneMove.targetScene = "GoToEnding";
+            sceneMove.ChangeScene();
+
         }
         // 스트레스가 100 이상일 경우 게임오버
         if (PlayerPrefs.GetInt("Stress") >= 100)
         {
-            GoToGameOver();
+            SceneMove sceneMove = gameObject.AddComponent<SceneMove>();
+            sceneMove.targetScene = "Ending-GameOver";
+            sceneMove.ChangeScene();
         }
         funitureController.UpdateFurnitures();
         statusController.UpdateStatus();
@@ -33,47 +38,6 @@ public class MainUpdateController : MonoBehaviour
         funitureController.UpdateFurnitures();
         statusController.UpdateStatus();
         phonePanel.SetActive(false);
-    }
-
-    public void GoToEnding() // 엔딩으로
-    {
-        int money = PlayerPrefs.GetInt("Money");
-        int myFame = PlayerPrefs.GetInt("MyFame");
-        int bandFame = PlayerPrefs.GetInt("Fame");
-        string endingScene = "";
-
-        SceneMove sceneMove = gameObject.AddComponent<SceneMove>();
-
-        if (money >= 2500000)
-        {
-            endingScene = "Ending-Expedition";
-        }
-        else if (myFame >= 100)
-        {
-            endingScene = "Ending-OpeningBand";
-        }
-        else if (bandFame >= 300)
-        {
-            endingScene = "Ending-ConcertInKorea";
-        }
-        else
-        {
-            endingScene = "Ending-Normal";
-        }
-
-        // 엔딩 잠금 해제 및 저장
-        PlayerPrefs.SetString("Ending", endingScene);
-        sceneMove.targetScene = endingScene;
-        sceneMove.ChangeScene();
-    }
-
-    public void GoToGameOver()
-    {
-
-        
-        SceneMove sceneMove = gameObject.AddComponent<SceneMove>();
-        sceneMove.targetScene = "Ending-GameOver";
-        sceneMove.ChangeScene();
     }
 
 }
