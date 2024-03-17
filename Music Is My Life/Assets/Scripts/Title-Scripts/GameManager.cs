1ba5a140087b62683989d5b00a5fc3d8968e6a6c
seohyun-lee <applemint2003@gmail.com>
2024-03-17 17:23:03 +0900

    ✨Feat: TitleScreen에서 새 게임, 이어하기, 데이터 리셋 기능 추가
    * Feat: TitleScreen에서 새 게임, 이어하기, 데이터 리셋 기능 추가 (GameManager 스크립트 리팩토링)
    * Fix: 나가기(Exit)의 behaviorId 7로 설정

--- Music Is My Life/Assets/Scripts/Title-Scripts/GameManager.cs
+++ Music Is My Life/Assets/Scripts/Title-Scripts/GameManager.cs
@@ -6,13 +6,37 @@ public class GameManager : MonoBehaviour
     public TMP_InputField nameInputField;
     public GameObject startPanel;
     public GameObject htpPanel;
-    private InitializeManager initializeManager = new InitializeManager();
+    public GameObject htpBtn;
+    public GameObject continueBtn;
+    private InitializeManager initializeManager;
+    private SceneMove sceneMove;
 
     void Start()
     {
         startPanel.SetActive(false);
         htpPanel.SetActive(false);
+        if (PlayerPrefs.GetInt("Dday") == 0) {
+            htpBtn.SetActive(true);
+            continueBtn.SetActive(false);
+        }
+        else
+        {
+            htpBtn.SetActive(false);
+            continueBtn.SetActive(true);
+        }
+        initializeManager = gameObject.AddComponent<InitializeManager>();
+        sceneMove = gameObject.AddComponent<SceneMove>();
+    }
+
+    public void NewGame() // 새 게임
+    {
         initializeManager.InitializeGamedata();
+    }
+
+    public void ContinueGame() // 이어하기: Main으로 씬 이동
+    {
+        sceneMove.targetScene = "Main";
+        sceneMove.ChangeScene();
     }
 
     public void SavePlayerName()
@@ -28,7 +52,6 @@ public class GameManager : MonoBehaviour
             Debug.Log("PlayerName: "+playerName);
             PlayerPrefs.SetString("PlayerName", playerName);
             // 프롤로그로 씬 이동
-            SceneMove sceneMove = gameObject.AddComponent<SceneMove>();
             sceneMove.targetScene = "Prologue";
             sceneMove.ChangeScene();
         }
@@ -37,14 +60,17 @@ public class GameManager : MonoBehaviour
             Debug.Log("PlayerName이 1~10자가 아닙니다. " + playerName);
         }
     }
+
     public void QuitGame() // 게임 종료 버튼
     {
         Application.Quit();
     }
 
-    public void ResetGame() // 리셋 버튼
+    public void ResetGame() // 게임 리셋: 엔딩컬렉션도 초기화
     {
         EndingCollectionManager.ResetEndingCollection();
         initializeManager.InitializeGamedata();
+        htpBtn.SetActive(true);
+        continueBtn.SetActive(false);
     }
 }
