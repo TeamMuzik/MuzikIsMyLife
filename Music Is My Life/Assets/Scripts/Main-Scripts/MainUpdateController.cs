9ed1b3f62cc14da50c7871c0c932dc222c7762e9
seohyun-lee <applemint2003@gmail.com>
2024-06-30 13:02:02 +0900

    fix: MainUpdateController 충돌사항 해결

--- Music Is My Life/Assets/Scripts/Main-Scripts/MainUpdateController.cs
+++ Music Is My Life/Assets/Scripts/Main-Scripts/MainUpdateController.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using UnityEngine;
+
 public class MainUpdateController : MonoBehaviour
 {
     public FunitureController funitureController;
     public StatusController statusController;
+    public SNSController snsController;
     public GameObject roomPanel;
     public GameObject phonePanel;
     public GameObject storePanel;
@@ -15,20 +17,31 @@ public class MainUpdateController : MonoBehaviour
         storePanel.SetActive(false);
         StatusChanger.UpdateDay(); // 날짜 업데이트
 
+        // 시즌이 새로 시작될 때 데이터 리셋
+        if (PlayerPrefs.GetInt("Dday") == 1)
+        {
+            SpriteUtils.ResetAllSavedSprites();
+        }
+
         // 14일이 지나면 15일: 엔딩으로 이동
         if (PlayerPrefs.GetInt("Dday") >= PlayerPrefs.GetInt("EndDday")) // EndDday: 15
         {
-            SceneMove sceneMove = gameObject.AddComponent<SceneMove>();
-            sceneMove.targetScene = "Nar-GoToEnding";
-            sceneMove.ChangeScene();
-
+            snsController.ShowDayStartPanel(() => {
+                SceneMove sceneMove = gameObject.AddComponent<SceneMove>();
+                sceneMove.targetScene = "Nar-GoToEnding";
+                sceneMove.ChangeScene();
+            });
         }
         // 스트레스가 100 이상일 경우 게임오버
-        if (PlayerPrefs.GetInt("Stress") >= 100)
+        else if (PlayerPrefs.GetInt("Stress") >= 100)
         {
             GoToGameOver();
         }
-        statusController.UpdateStatus();
+        else
+        {
+            statusController.UpdateStatus(); // 상태창 업데이트
+            snsController.ShowDayStartPanel(); // 하루 시작 패널 표시
+        }
     }
 
     public void UpdateAfterStore() // Store Panel의 ExitBtn에서 호출
