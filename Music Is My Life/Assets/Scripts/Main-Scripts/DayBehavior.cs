ca6a3baa7c021cf70aedfb405de9902347b0cbb8
HongYeonLee <llhongyan@naver.com>
2024-09-12 23:35:22 +0900

    fix: 알바 연속 횟수 변수 초기화 수정

--- Music Is My Life/Assets/Scripts/Main-Scripts/DayBehavior.cs
+++ Music Is My Life/Assets/Scripts/Main-Scripts/DayBehavior.cs
@@ -9,7 +9,11 @@ public class DayBehavior : MonoBehaviour
         int d = PlayerPrefs.GetInt("Dday");
         PlayerPrefs.SetInt("Day" + d + "_Behavior", behaviorId);
         Debug.Log("SaveTodayBehavior:" + behaviorId);
-        if (behaviorId < 3)  // 선택한 app이 알바이면, 알바 연속 횟수 변수 증가
+        if (behaviorId > 2 && behaviorId != 6){ // 선택한 app이 알바가 아니고, 상점이 아닌경우, 알바 연속 횟수 변수 0으로 초기화
+            PlayerPrefs.SetInt("PartTimeContinuity", 0);
+        }
+
+        else if (behaviorId < 3)  // 선택한 app이 알바이면, 알바 연속 횟수 변수 증가
         {
             int count = PlayerPrefs.GetInt("PartTimeContinuity");
             PlayerPrefs.SetInt("PartTimeContinuity", count + 1);
