786953b4f3de3017bff6b27d0bf4748472eadae4
HongYeonLee <llhongyan@naver.com>
2024-09-22 19:45:53 +0900

    fix: 이벤트-아티스트 투표에서 금메달 이미지 비활성화 문제 해결

--- Music Is My Life/Assets/Scripts/Event-Scripts/EventAvController.cs
+++ Music Is My Life/Assets/Scripts/Event-Scripts/EventAvController.cs
@@ -55,6 +55,7 @@ public class EventAvController : MonoBehaviour
             nextButton.SetActive(false);
             scorePanel.SetActive(true);
             rivalWinPrize.SetActive(false);
+            bandWinPrize.SetActive(false);
         }
     }
 
